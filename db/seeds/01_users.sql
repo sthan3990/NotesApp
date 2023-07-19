24a59667fe0eeed5e9ae3cd01bf395bd75846397
phankons <stephen.han@gmx.com>
2023-07-19 07:55:01 -0400

    improve user seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here (Example)
--- Password is p@ssw0rd
+-- Password is p@ssw0rd (bcrypt encrypted hash)
 INSERT INTO users (username, password, email)
 VALUES ('Alice', '$2a$12$S6dUOM7SiIwxiwYVL.epB.yAtKd3HobbB77iK56IATMjmFieq75h2', 'example@gmail.com');
 
