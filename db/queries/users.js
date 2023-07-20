cceaaee3dad2b0b0b1020738121f434e4b5961a8
phankons <stephen.han@gmx.com>
2023-07-19 23:15:48 -0400

    Create Add User Feature

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,6 +7,7 @@ const getUsers = () => {
     });
 };
 
+<<<<<<< HEAD
 const insertUser = (name, password, email) => {
   return db.query(`INSERT INTO users (name, password, email)
        VALUES ($1, $2, $3)
@@ -21,3 +22,6 @@ const insertUser = (name, password, email) => {
 
 
 module.exports = { getUsers, insertUser};
+=======
+module.exports = { getUsers };
+>>>>>>> 25ffd24 (Create Add User Feature)
--- routes/users-api.js
+++ routes/users-api.js
@@ -1,3 +1,32 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+/*
+ * All routes for User Data are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /api/users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const userQueries = require('../db/queries/users');
+
+router.get('/', (req, res) => {
+  userQueries.getUsers()
+    .then(users => {
+      res.json({ users });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+module.exports = router;
+=======
+>>>>>>> 25ffd24 (Create Add User Feature)
 /*
  * All routes for User Data are defined here
  * Since this file is loaded in server.js into api/users,
@@ -39,17 +68,30 @@ router.post('/register', (req, res) => {
   // check if email is already used
   let userExists = 0;
 
+<<<<<<< HEAD
   for (let user in usersArray) {
     if (user === req.body.email) {
+=======
+  foreach(user in usersArray) {
+    if (user == req.body.email) {
+>>>>>>> 25ffd24 (Create Add User Feature)
       userExists = 1;
     }
   }
 
+<<<<<<< HEAD
   if (userExists !== 1) {
     try {
       const email = req.body.email;
       const name = req.body.name;
       const salt = bcrypt.genSaltSync(10);
+=======
+  if (userExists != 1) {
+    try {
+      const email = req.body.email
+      const name = req.body.name;
+      const salt = bcrypt.genSaltSync(3);
+>>>>>>> 25ffd24 (Create Add User Feature)
 
       // use salt to hash password
       const password = bcrypt.hashSync(req.body.password, salt);
@@ -68,3 +110,7 @@ router.post('/register', (req, res) => {
 });
 
 module.exports = router;
+<<<<<<< HEAD
+=======
+>>>>>>> d2a91f8 (Create Add User Feature)
+>>>>>>> 25ffd24 (Create Add User Feature)
