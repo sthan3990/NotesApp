295fef2d2e4f71326173fdf56a6b8311fef1229a
phankons <stephen.han@gmx.com>
2023-07-19 22:48:34 -0400

    Create Schemas and Seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,8 @@
 -- Users table seeds here (Example)
 -- Password is p@ssw0rd (bcrypt encrypted hash)
-INSERT INTO users (username, password, email)
-VALUES ('Alice', '$2a$12$S6dUOM7SiIwxiwYVL.epB.yAtKd3HobbB77iK56IATMjmFieq75h2', 'example@gmail.com');
 
-INSERT INTO users (username, password, email)
-VALUES ('Bob', '$2a$12$S6dUOM7SiIwxiwYVL.epB.yAtKd3HobbB77iK56IATMjmFieq75h2', 'example2@gmail.com');
+INSERT INTO users (id, username, password, email)
+VALUES ('1', 'Alice', '$2a$12$S6dUOM7SiIwxiwYVL.epB.yAtKd3HobbB77iK56IATMjmFieq75h2', 'example@gmail.com');
+
+INSERT INTO users (id, username, password, email)
+VALUES ('1', 'Bob', '$2a$12$S6dUOM7SiIwxiwYVL.epB.yAtKd3HobbB77iK56IATMjmFieq75h2', 'example2@gmail.com');
