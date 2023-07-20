6042f88c03b9fe512a6dbc1de05c58d08eefdb61
phankons <stephen.han@gmx.com>
2023-07-19 23:15:48 -0400

    Create Add User Feature

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,20 +7,4 @@ const getUsers = () => {
     });
 };
 
-const insertUser = (name, password, email) => {
-  return db.query(`INSERT INTO users (name, password, email)
-       VALUES ($1, $2, $3)
-       RETURNING *;`, [name, password, email])
-    .then((result) => {
-      return result.rows[0];
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 578b71b (Implemented database.js Adding queries for login, register, Also fixed)
-module.exports = { getUsers, insertUser};
+module.exports = { getUsers };
--- routes/users-api.js
+++ routes/users-api.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 /*
  * All routes for User Data are defined here
  * Since this file is loaded in server.js into api/users,
@@ -9,33 +7,6 @@
 
 const express = require('express');
 const router  = express.Router();
-const userQueries = require('../db/queries/users');
-
-router.get('/', (req, res) => {
-  userQueries.getUsers()
-    .then(users => {
-      res.json({ users });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-});
-
-module.exports = router;
-=======
-=======
->>>>>>> 68e77f9 (Implemented database.js Adding queries for login, register, Also fixed)
-/*
- * All routes for User Data are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /api/users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
-const express = require('express');
-const router = express.Router();
 const userQueries = require('../db/queries/users');
 const bcrypt = require("bcrypt");
 
@@ -51,79 +22,4 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/register', (req, res) => {
-  let usersArray = [];
-
-  // grab all user emails
-  userQueries.getUsers()
-    .then(users => {
-      usersArray.push(users);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-
-  // check if email is already used
-  let userExists = 0;
-
-  for (let user in usersArray) {
-    if (user === req.body.email) {
-      userExists = 1;
-    }
-  }
-
-  if (userExists !== 1) {
-    try {
-      const email = req.body.email;
-      const name = req.body.name;
-      const salt = bcrypt.genSaltSync(10);
-
-      // use salt to hash password
-      const password = bcrypt.hashSync(req.body.password, salt);
-
-      // insert into database
-      userQueries.insertUser(name, password, email);
-    }
-    catch (err) {
-      console.log(err);
-    }
-
-  }
-
-
-
-});
-
 module.exports = router;
-<<<<<<< HEAD
->>>>>>> d2a91f8 (Create Add User Feature)
-=======
-=======
-/*
- * All routes for User Data are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /api/users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
-const express = require('express');
-const router  = express.Router();
-const userQueries = require('../db/queries/users');
-
-router.get('/', (req, res) => {
-  userQueries.getUsers()
-    .then(users => {
-      res.json({ users });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-});
-
-module.exports = router;
->>>>>>> 578b71b (Implemented database.js Adding queries for login, register, Also fixed)
->>>>>>> 68e77f9 (Implemented database.js Adding queries for login, register, Also fixed)
