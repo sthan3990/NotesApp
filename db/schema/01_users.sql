ca0999628baed9214ab8ea2ccffd5c3a982d9b90
Saba Oji <sabaoji@gmail.com>
2023-07-19 21:07:39 -0400

    Created the schema and seeds for the SQL files

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
+-- Drop and recreate Users table (Example)
 DROP TABLE IF EXISTS users CASCADE;
-
-CREATE TABLE Users (
-    user_id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL
 );
-=======
-CREATE TABLE Users (
-    user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-);
->>>>>>> 578b71b (Implemented database.js Adding queries for login, register, Also fixed)
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,3 @@
-INSERT INTO users (username, password, email)
-VALUES
+INSERT INTO Users (username, password, email) VALUES
 ('user1', 'password1', 'user1@example.com'),
-('user2', 'password2', 'user2@example.com'),
-('user3', 'password3', 'user3@example.com'),
-('user4', 'password4', 'user4@example.com');
-
->>>>>>> 1437246 (fix seeds and schema syntax, added more values)
+('user2', 'password2', 'user2@example.com');
