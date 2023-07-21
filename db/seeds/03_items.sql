941437eca0e26b6cd9dbaf568cf845502690554b
swathij943 <swathij943@gmail.com>
2023-07-21 13:28:51 -0400

    Implementing styles on login

---
+++ db/seeds/03_items.sql
@@ -0,0 +1,5 @@
+INSERT INTO Items (item_name, category_id, user_id, status) VALUES
+('The Godfather', 1, 1, 'To watch'),
+('The Coffee Shop', 2, 1, 'To eat'),
+('1984', 3, 1, 'To read'),
+('New Laptop', 4, 1, 'To buy');
