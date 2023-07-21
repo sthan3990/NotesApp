2ed4d589e6236628e74917cd55ed54633ab9ee75
swathij943 <swathij943@gmail.com>
2023-07-21 11:01:11 -0400

    db schemas & seeds misaligned - sorted schemas & seeds

--- db/schema/02_categories.sql
+++ db/schema/02_categories.sql
@@ -3,4 +3,3 @@ CREATE TABLE categories (
     id SERIAL NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
->>>>>>> 1437246 (fix seeds and schema syntax, added more values)
--- db/schema/03_items.sql
+++ db/schema/03_items.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Items (
     item_id SERIAL PRIMARY KEY,
-    item_name VARCHAR(100) NOT NULL,
+    item_name VARCHAR(255) NOT NULL,
     category_id INTEGER REFERENCES Categories(category_id),
     user_id INTEGER REFERENCES Users(user_id),
     status VARCHAR(20) NOT NULL,
