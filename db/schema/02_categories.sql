32cabd7cd73b9e8b0b2f46b323f25ac7cfafa909
swathij943 <swathij943@gmail.com>
2023-07-21 11:01:11 -0400

    db schemas & seeds misaligned - sorted schemas & seeds

--- db/schema/02_categories.sql
+++ db/schema/02_categories.sql
@@ -2,9 +2,5 @@ DROP TABLE IF EXISTS categories CASCADE;
 
 CREATE TABLE categories (
     id SERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
-<<<<<<< HEAD
+    name VARCHAR(255) NOT NULL
 );
-=======
-);
->>>>>>> 1437246 (fix seeds and schema syntax, added more values)
