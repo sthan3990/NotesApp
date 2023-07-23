2d1ae437ec0922679ca4c5dc7cf47f9900cd49ce
swathij943 <swathij943@gmail.com>
2023-07-23 19:29:17 -0400

    Implementing styles on login

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -2,5 +2,5 @@ CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL
 );
