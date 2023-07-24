a1dadaca236c6d824429b96e93a6355440bd48b9
Saba Oji <sabaoji@gmail.com>
2023-07-24 10:48:39 -0400

    Created a new file under routes called auth.js which includes the code for register/login/logout. I also had to update some other files to make sure it works, those include server.js and package.json and a few other

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+<<<<<<< HEAD
 const helmet = require('helmet');
 const cookieSession = require('cookie-session');
 const { updateuserProfile, getuserProfile } = require('./db/queries/profile');
 const bcrypt = require('bcrypt');
+=======
+const cookieSession = require('cookie-session');
+>>>>>>> 359e39a (Created a new file under routes called auth.js which includes the code for register/login/logout. I also had to update some other files to make sure it works, those include server.js and package.json and a few other)
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -34,6 +38,10 @@ app.use(cookieSession({
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
 app.use(
   '/styles',
   sassMiddleware({
@@ -49,8 +57,12 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+<<<<<<< HEAD
 const registerRoutes = require('./routes/register');
 const loginRoutes = require('./routes/login');
+=======
+const authRoutes = require('./routes/auth');
+>>>>>>> 359e39a (Created a new file under routes called auth.js which includes the code for register/login/logout. I also had to update some other files to make sure it works, those include server.js and package.json and a few other)
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -58,8 +70,12 @@ const loginRoutes = require('./routes/login');
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+<<<<<<< HEAD
 app.use('/register', registerRoutes());
 app.use('/login', loginRoutes());
+=======
+app.use('/auth',authRoutes);
+>>>>>>> 359e39a (Created a new file under routes called auth.js which includes the code for register/login/logout. I also had to update some other files to make sure it works, those include server.js and package.json and a few other)
 // Note: mount other resources here, using the same pattern above
 
 // Home page
