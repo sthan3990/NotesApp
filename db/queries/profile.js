a9cd8ab503e734681029abe8001455691949ed33
phankons <stephen.han@gmx.com>
2023-07-24 15:28:09 -0400

    Create Update Profile Feature

--- db/queries/profile.js
+++ db/queries/profile.js
@@ -1,8 +1,7 @@
 const db = require('../connection');
 
 const getuserProfile = (email) => {
-  return db.query(`SELECT * FROM users WHERE email = $1
-       RETURNING *;`, [email])
+  return db.query(`SELECT * FROM users WHERE email = $1;`, [email])
     .then((result) => {
       return result.rows[0];
     })
@@ -11,4 +10,17 @@ const getuserProfile = (email) => {
     });
 };
 
-module.exports = { getuserProfile};
+const updateuserProfile = (name, email, password) => {
+  return db.query(`UPDATE users SET name = $1, email = $2, password = $3
+       WHERE users.email = $2
+       RETURNING *;`, [name, email, password])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+
+module.exports = { getuserProfile, updateuserProfile};
