99d87fa4bec2943b225b1b2e87b534069b9ec5f3
phankons <stephen.han@gmx.com>
2023-07-26 21:00:51 -0400

    Create task insert query

--- db/queries/tasks.js
+++ db/queries/tasks.js
@@ -1,8 +1,9 @@
 const db = require('../connection');
 
-const insertTask = (email, category) => {
-  return db.query(`INSERT INTO tasks(task, category, date) VALUES ($1, $2)
-    RETURNING *;`, [email, category])
+
+const insertTask = (message, category, userID, completed, theDate) => {
+  return db.query(`INSERT INTO tasks(name, category_id, user_id, completed, date) VALUES ($1, $2, $3, $4, $5)
+    RETURNING *;`, [message, category, userID, completed, theDate])
     .then((result) => {
       return result.rows[0];
     })
