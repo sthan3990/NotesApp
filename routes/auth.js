4a0f13066852f3bb93f2bd910cfaf24038e38910
phankons <stephen.han@gmx.com>
2023-07-29 10:34:32 -0400

    Working Example

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const promise = require("bluebird");
 const taskQueries = require("../db/queries/tasks");
+const userQueries = require("../db/queries/users");
 
 const initOptions = {
   promiseLib: promise,
@@ -15,34 +16,19 @@ function generateRandomString() {
   return Math.random().toString(36).substring(2, 8);
 }
 
-router.get("/", (req, res) => {
-  const userID = req.session.user_id;
-
-  if (!userID) {
-    res.render("login", { user: null });
-
-  } else {
-    taskQueries
-      .getAllTasks(userID)
-      .then(({ allTasks }) => {
-        res.render("index", { allTasks });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  }
-});
-
 // Registration Page
 router.get("/register", (req, res) => {
-  res.render("register");
+  const userID = req.session.user_id;
+  userQueries.getUsername(userID).then(({ username }) => {
+    res.render("register", { username, user: userID });
+  });
 });
 
 // Creating a new user
 router.post("/register", (req, res) => {
   const email = req.body.email;
-  const username = req.body.username;
   const password = bcrypt.hashSync(req.body.password, 10);
+  const username = req.body.username;
   if (!email || !password) {
     res.status(400).send("Please provide both a valid email and password");
   } else {
@@ -51,25 +37,17 @@ router.post("/register", (req, res) => {
         if (results.rows.length > 0) {
           res.status(400).send("This account already exists");
         } else {
-          // create the user
           db.query(
-            "INSERT INTO users(username, password, email) VALUES($1, $2, $3) RETURNING *",
+            "INSERT INTO users(username, password, email) VALUES($1, $2, $3) RETURNING id",
             [username, password, email]
           )
             .then((response) => {
               req.session.user_id = response.rows[0].id;
-
-              let today = Date.now();
-              today.getUTCDate();
-
-              // create dummy tasks
-              db.query(
-                "INSERT INTO tasks (name, category_id, user_id, completed, date) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-                ["example", "1", "1", "FALSE", today]
-              )
-                .then((response) => {
-                  res.redirect("/"); // Redirect to user's profile
-                });
+              res.redirect("/"); // Redirect to user's profile
+            })
+            .catch((error) => {
+              // handle error
+              console.log(error);
             });
         }
       })
@@ -80,24 +58,25 @@ router.post("/register", (req, res) => {
   }
 });
 
-// Login
-router.get("/login", function (req, res) {
-
-  // user is not logged in, so pass null for user
-  res.render("login", { user: null });
-
+// render login page
+router.get("/login", (req, res) => {
+  const userID = req.session.user_id;
+  userQueries.getUsername(userID).then(({ username }) => {
+    res.render("login", { username, user: userID });
+  });
 });
 
+// post login page
 router.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+
   // Look up the user by email
   db.query("SELECT * FROM users WHERE email = $1", [email])
     .then((results) => {
       if (results.rows.length <= 0) {
         return res.status(400).send("Email account does not exist");
       }
-
       const user = results.rows[0];
       // Check to see if the user exists and if the password matches
       if (!user || !bcrypt.compareSync(password, user.password)) {
@@ -118,11 +97,25 @@ router.post("/login", (req, res) => {
 
 // Logout
 router.post("/logout", (req, res) => {
-  res.clearCookie();
   req.session = null;
   res.redirect("/login");
 });
 
-
+// render index page with logged in users tasks
+router.get("/", (req, res) => {
+  const userID = req.session.user_id;
+  taskQueries
+    .getAllTasks(userID)
+    .then(({ allTasks, username }) => {
+      res.render("index", {
+        allTasks,
+        user: userID,
+        username,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
 
 module.exports = router;
